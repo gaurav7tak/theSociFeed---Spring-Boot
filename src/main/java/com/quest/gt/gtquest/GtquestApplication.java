cc661d4479c08437fbb4b1d01d138b4523e6ed65
Gaurav Tak <gaurav.tak@metacube.com>
2019-05-23 18:16:25 +0530

    First commit Spring Boot app

---
+++ src/main/java/com/quest/gt/gtquest/GtquestApplication.java
@@ -0,0 +1,13 @@
+package com.quest.gt.gtquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtquestApplication.class, args);
+	}
+
+}
